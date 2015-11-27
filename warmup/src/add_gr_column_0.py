78a0947d81b2f62c6c7fbe8932c0df2a555a06a5
Gabriel F. Berriz <gabriel_berriz@hms.harvard.edu>
2015-11-27 06:26:13 -0500

    use clearer notation in python code

--- warmup/src/add_gr_column_0.py
+++ warmup/src/add_gr_column_0.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 import csv
-import fileinput as fi
-import collections as co
-import math as ma
+import fileinput
+import collections
+import math
 import decimal
 
 # ------------------------------------------------------------------------------
-log2 = lambda x: ma.log(x, 2)
+log2 = lambda x: math.log(x, 2)
 
 def normalize_log2(n, n_0_0):
     assert isinstance(n, float) or isinstance(n_0_0, float)
@@ -32,20 +32,20 @@ def main():
       cell_count__time0
 
     '''
-    reader = csv.reader(fi.input(mode='rb'), delimiter='\t')
+    reader = csv.reader(fileinput.input(mode='rb'), delimiter='\t')
 
     headers = next(reader)
-    to_record = co.namedtuple(typename='record',
-                              field_names=headers,
-                              rename=True)
+    to_record = collections.namedtuple(typename='record',
+                                       field_names=headers,
+                                       rename=True)
 
     def adjust_sigfigs(val, _context=decimal.Context(prec=3)):
         return _context.create_decimal(val)
 
     for r in (to_record(*row) for row in reader):
         cell_count__time0 = float(r.cell_count__time0)
-        log2nn, log2nn_ctrl = (normalize_log2(float(n), cell_count__time0)
-                               for n in (r.cell_count, r.cell_count__ctrl))
+        log2nn, log2nn_ctrl = (normalize_log2(float(count), cell_count__time0)
+                               for count in (r.cell_count, r.cell_count__ctrl))
 
         gr = 2**(log2nn/log2nn_ctrl) - 1
 
